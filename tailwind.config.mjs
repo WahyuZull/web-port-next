672ba6934ab4073b5bf238de673f2317397f8686
Wahyu Zull <wahyuzuliyono@gmail.com>
2025-01-16 21:42:34 +0700

    Add components directory path to Tailwind config content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
